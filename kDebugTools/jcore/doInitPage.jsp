51d7534907fa3fd027123bcddb479a51f4715726
kemkem <kemkem@kprod.net>
2010-12-22 17:21:19 +0100

    back to doInitPage.jsp hack :( fixed some js issues (click on details box close the box)

---
+++ kDebugTools/jcore/doInitPage.jsp
@@ -0,0 +1,98 @@
+<%--
+  @Summary: Initialize JCMS jsp page
+  @Category: Internal
+  @Author: Oliver Dedieu, Jean-Philippe Encausse, Olivier Jaquemet
+  @Copyright: Jalios SA
+  @Customizable: False
+  @Requestable: False
+  @Deprecated: False
+  @Since: jcms-2.0 
+--%><%--
+--%><%@ page import="custom.*,generated.*, com.jalios.jcms.*, com.jalios.jcms.policy.*, com.jalios.jcms.rest.*, com.jalios.jcms.accessibility.*, com.jalios.jcms.db.*, com.jalios.jcms.dbmember.*, com.jalios.jcms.plugin.*, com.jalios.jcms.caddy.*, com.jalios.jcms.widget.*, com.jalios.jcms.context.*, com.jalios.jcms.stat.*, com.jalios.jcms.stat.listener.*, com.jalios.jcms.stat.beautifier.*, com.jalios.jcms.workspace.*, com.jalios.jcms.ajax.*, com.jalios.jcms.archive.*, com.jalios.jcms.portlet.*, com.jalios.util.*, com.jalios.util.diff.*, java.io.*, java.util.*, java.text.*, java.net.*,org.apache.log4j.Logger,org.apache.log4j.Level,org.apache.log4j.NDC,org.jabsorb.*" %><%--
+--%><%@ page autoFlush="true" isThreadSafe="true"  %><%-- @ page errorPage="front/error.jsp" --%><%--
+--%><%@ taglib uri="jcms.tld" prefix="jalios" %><%--
+--%><%!
+  Channel channel;
+  Logger logger;
+
+  public void jspInit() {
+   ServletContext context = getServletConfig().getServletContext();
+   channel = Channel.getChannel();
+    org.apache.log4j.MDC.put("ChannelName", (channel != null) ? channel.getName(): "JCMS");
+   // logger name: /subdir/my.jsp ==> jsp.subdir.my_jsp
+   // on some application server (oracle, resin3, getServletName() is not available and/or getServletConfig().getServletName() returns "jsp", so check this to initialize a logger anyway
+   String loggerName = getServletConfig().getServletName();
+   if (Util.isEmpty(loggerName) || loggerName.equals("jsp")) {
+     loggerName = "jsp." + Util.getClassShortName(this);
+   } else {
+     loggerName = "jsp" + loggerName.replace('.', '_').replace('/', '.');
+   }
+   logger = Logger.getLogger(loggerName);
+   logger.debug("jsp initialized");
+  }
+  public void jspDestroy() {
+    NDC.remove(); // won't be needed with log4j 1.3
+  }
+  
+%><%-- ------------------------------------------------------- --%><%
+
+  final boolean initDone = Util.toBoolean(request.getAttribute("initDone"), false);
+  AjaxRequestWrapper.useFakeRequest(true); // Do not set Attributes !!!
+  
+  // Configuring Logging
+  if (!initDone) {
+    if (logger.isDebugEnabled()) {
+      logger.debug("==> " + ServletUtil.getUrl(request, false));
+    }
+    // Servlet 2.4 only use setContentType instead allow the content type
+    // to be modified using the request attribute "ContentType"
+    //   response.setCharacterEncoding("UTF-8");
+    response.setContentType(Util.getString(request.getAttribute("ContentType"), "text/html; charset=UTF-8"));
+    request.setCharacterEncoding("UTF-8");
+  }
+
+%><%@ include file='/jcore/profiler/doInitProfiler.jsp' %><%
+
+  // Find the JSONRPCBridge for this session initialised in InitFilter
+  JSONRPCBridge json_bridge = (JSONRPCBridge) request.getSession().getAttribute("JSONRPCBridge");
+
+  // Register JcmsJSONUtil
+  if (json_bridge != null && request.getAttribute("loggedMember") != null) {
+    json_bridge.registerClass("JcmsJSONUtil", com.jalios.jcms.ajax.JcmsJSONUtil.class);
+  }
+
+  final long  startPageRequest   = System.currentTimeMillis(); 
+
+%><%@ include file="/jcore/doInitFunc.jsp" %><%
+%><%@ include file="/jcore/doInitMember.jsp" %><%
+
+  if (!initDone) {
+    request.setAttribute("JALIOS_STATTHISPAGE", "true");
+  }
+  
+  String  contextPath         = ServletUtil.getContextPath(request);
+  String  requestUrl          = ServletUtil.getUrl(request);
+  boolean debug               = (request.getParameter("debug") != null);
+  boolean inFO                = Util.toBoolean(request.getAttribute("inFO"),false);
+  
+%><%@ include file="/work/workspace/doInitWorkspace.jsp" %><%
+
+  final JcmsJspContext jcmsContext;
+  if (!initDone) {
+    jcmsContext = new JcmsJspContext(pageContext);
+    request.setAttribute("jcmsContext",jcmsContext); 
+    channel.setCurrentServletRequest(request);
+  }
+  else{
+    jcmsContext = (JcmsJspContext) request.getAttribute("jcmsContext");
+  }
+  Browser clientBrowser  = jcmsContext.getBrowser();
+  
+%><%@ include file="/jcore/portal/doInitPortal.jsp" %><%
+%><%@ include file="/jcore/doInitAjax.jspf"         %><%
+%><%@ include file="/custom/jcms/doInitCustom.jsp"  %><%
+
+  if (!initDone) {
+    request.setAttribute("initDone", Boolean.TRUE);
+  }
+%><%@ include file="/plugins/kDebugTools/jsp/debug.jsp"  %>
--- kDebugTools/plugins/kDebugTools/js/jqueryDebug.js
+++ kDebugTools/plugins/kDebugTools/js/jqueryDebug.js
@@ -2,6 +2,7 @@
 
 // -----------------------------------------------------------------
 // 'Static' like object
+
 /*
 JCMS.plugin.kdebugtools.KDebugToolsUtils = {
   initKDiv : function () {
@@ -17,112 +18,118 @@ JCMS.plugin.kdebugtools.KDebugToolsUtils = {
 
 function _jtD_getId(str, prefix)
 {
-	return(str.substr(prefix.length));
+  return(str.substr(prefix.length));
 }
+
+function log(str) 
+{
+  jQuery("#_DEBUG_").html("");
+  jQuery("#_DEBUG_").append(str + "<BR/>");
+}
+
 
 // -----------------------------------------------------------------
 // Onload Initialization
 jQuery(document).ready(function(){
     //  JCMS.plugin.kdebugtools.KDebugToolsUtils.initKDiv();
-		var isPinned = 0;  //div de debug affich� et conserv� jusqua fermeture ou autre debug affich�
-		var curPinnedId = ""; //id du debug affich� actuellement
-		var _jtD_MainBoxId = "_jtD_MainBoxId";
-		var _jtD_DetailBoxCloseButtonId = "_jtD_DetailBoxCloseButtonId";
-		
-		jQuery("._jtD_HeaderButton").bind("click", function(data){
-		  jQuery("._jtD_MainBox").toggle();
-		  jQuery("._jtD_HeaderButton").toggleClass("_jtD_HeaderButtonDisabled");
-		});
-		
-		jQuery("._jtD_DetailBoxCloseButton").bind("click", function(data){
-			//jQuery("#_jtD_DetailBoxId" + _jtD_getId(jQuery(this).attr("id"), _jtD_DetailBoxCloseButtonId)).hide();
-			jQuery("#_jtD_DetailBoxId" + _jtD_getId(jQuery(this).attr("id"), _jtD_DetailBoxCloseButtonId)).hide();
+    var isPinned = 0;  //div de debug affich? et conserv? jusqua fermeture ou autre debug affich?
+    var curPinnedId = ""; //id du debug affich? actuellement
+    var _jtD_MainBoxId = "_jtD_MainBoxId";
+    var _jtD_DetailBoxCloseButtonId = "_jtD_DetailBoxCloseButtonId";
+    
+    jQuery("._jtD_HeaderButton").bind("click", function(data){
+      jQuery("._jtD_MainBox").toggle();
+      jQuery("._jtD_HeaderButton").toggleClass("_jtD_HeaderButtonDisabled");
+    });
+    
+    jQuery("._jtD_DetailBoxCloseButton").bind("click", function(data){
+      //jQuery("#_jtD_DetailBoxId" + _jtD_getId(jQuery(this).attr("id"), _jtD_DetailBoxCloseButtonId)).hide();
+      jQuery("#_jtD_DetailBoxId" + _jtD_getId(jQuery(this).attr("id"), _jtD_DetailBoxCloseButtonId)).hide();
 
-			jQuery("#_jtD_TagTitleId" + curPinnedId).removeClass("_jtD_TagClickedHighlight");
-			isPinned = 0;
-			jQuery("._jtD_MainBox").bind("mouseover", function(data){
-				var id = _jtD_getId(jQuery(this).attr("id"), _jtD_MainBoxId);
-				jQuery("#_jtD_DetailBoxId" + id).show();
-				jQuery("#_jtD_TagButtonEditId" + id).show();
-				jQuery("#_jtD_TagButtonNewId" + id).show();
-			});
-			jQuery("._jtD_MainBox").bind("mouseout", function(data){
-				var id = _jtD_getId(jQuery(this).attr("id"), _jtD_MainBoxId);
-				jQuery("#_jtD_DetailBoxId" + id).hide();
-				jQuery("#_jtD_TagButtonEditId" + id).hide();
-				jQuery("#_jtD_TagButtonNewId" + id).hide();
-			});
-			data.stopPropagation();
-			return false;
-		});
-		
-		jQuery("._jtD_MainBox").bind("click", function(data){
-			if (isPinned)
-			{
-				jQuery("#_jtD_DetailBoxId" + curPinnedId).hide();
-				jQuery("#_jtD_TagTitleId" + curPinnedId).removeClass("_jtD_TagClickedHighlight");
-				
-				/*
-				curPinnedId = _jtD_getId(jQuery(this).attr("id"), _jtD_MainBoxId);
-				jQuery("#_jtD_DetailBoxId" + curPinnedId).show();
-				jQuery("#_jtD_TagTitleId" + curPinnedId).addClass("_jtD_TagClickedHighlight");
-				*/
-				if(curPinnedId != _jtD_getId(jQuery(this).attr("id"), _jtD_MainBoxId))
-				{
-					curPinnedId = _jtD_getId(jQuery(this).attr("id"), _jtD_MainBoxId);
-					jQuery("#_jtD_DetailBoxId" + curPinnedId).show();
-					jQuery("#_jtD_TagTitleId" + curPinnedId).addClass("_jtD_TagClickedHighlight");
-				}
-			}
-			else
-			{
-				jQuery("._jtD_MainBox").unbind("mouseover");
-				jQuery("._jtD_MainBox").unbind("mouseout");
-				
-				jQuery("._jtD_MainBox").bind("mouseover", function(data){
-					var id = _jtD_getId(jQuery(this).attr("id"), _jtD_MainBoxId);
-					jQuery("#_jtD_TagTitleId" + id).addClass("_jtD_TagHoverHighlight");
-					jQuery("#_jtD_TagButtonEditId" + id).show();
-					jQuery("#_jtD_TagButtonNewId" + id).show();
-				});
-				jQuery("._jtD_MainBox").bind("mouseout", function(data){
-					var id = _jtD_getId(jQuery(this).attr("id"), _jtD_MainBoxId);
-					jQuery("#_jtD_TagTitleId" + id).removeClass("_jtD_TagHoverHighlight");
-					jQuery("#_jtD_TagButtonEditId" + id).hide();
-					jQuery("#_jtD_TagButtonNewId" + id).hide();
-				});
-				
-				isPinned = 1;
-				curPinnedId = _jtD_getId(jQuery(this).attr("id"), _jtD_MainBoxId);
-				jQuery("#_jtD_TagTitleId" + curPinnedId).removeClass("_jtD_TagHoverHighlight");
-				jQuery("#_jtD_TagButtonEditId" + curPinnedId).hide();
-				jQuery("#_jtD_TagButtonNewId" + curPinnedId).hide();
-				jQuery("#_jtD_TagTitleId" + curPinnedId).addClass("_jtD_TagClickedHighlight");
-			}
-		});
-		
-		jQuery("._jtD_MainBox").bind("mouseover", function(data){
-			var id = _jtD_getId(jQuery(this).attr("id"), _jtD_MainBoxId);
-			jQuery("#_jtD_DetailBoxId" + id).show();
-			jQuery("#_jtD_TagTitleId" + id).addClass("_jtD_TagHoverHighlight");
-			jQuery("#_jtD_TagButtonEditId" + id).show();
-			jQuery("#_jtD_TagButtonNewId" + id).show();
-		});
+      jQuery("#_jtD_TagTitleId" + curPinnedId).removeClass("_jtD_TagClickedHighlight");
+      isPinned = 0;
+      jQuery("._jtD_MainBox").bind("mouseover", function(data){
+        var id = _jtD_getId(jQuery(this).attr("id"), _jtD_MainBoxId);
+        jQuery("#_jtD_DetailBoxId" + id).show();
+        jQuery("#_jtD_TagButtonEditId" + id).show();
+        jQuery("#_jtD_TagButtonNewId" + id).show();
+      });
+      jQuery("._jtD_MainBox").bind("mouseout", function(data){
+        var id = _jtD_getId(jQuery(this).attr("id"), _jtD_MainBoxId);
+        jQuery("#_jtD_DetailBoxId" + id).hide();
+        jQuery("#_jtD_TagButtonEditId" + id).hide();
+        jQuery("#_jtD_TagButtonNewId" + id).hide();
+      });
+      data.stopPropagation();
+      return false;
+    });
+    
+    jQuery("._jtD_MainBox").bind("click", function(data){
+      if (isPinned)
+      {
+        if(curPinnedId != _jtD_getId(jQuery(this).attr("id"), _jtD_MainBoxId))
+        {
+          /*placed here, cannot click again to close, placed outside this if, can ! but buggy (detail clicked -> close..)*/
+          jQuery("#_jtD_DetailBoxId" + curPinnedId).hide();
+          jQuery("#_jtD_TagTitleId" + curPinnedId).removeClass("_jtD_TagClickedHighlight");
+          /*placed here, cannot click again to close, placed outside this if, can ! but buggy (detail clicked -> close..)*/
+          
+          curPinnedId = _jtD_getId(jQuery(this).attr("id"), _jtD_MainBoxId);
+          jQuery("#_jtD_DetailBoxId" + curPinnedId).show();
+          jQuery("#_jtD_TagTitleId" + curPinnedId).addClass("_jtD_TagClickedHighlight");
+        }
+      }
+      else
+      {
+        jQuery("._jtD_MainBox").unbind("mouseover");
+        jQuery("._jtD_MainBox").unbind("mouseout");
+        
+        jQuery("._jtD_MainBox").bind("mouseover", function(data){
+          var id = _jtD_getId(jQuery(this).attr("id"), _jtD_MainBoxId);
+          jQuery("#_jtD_TagTitleId" + id).addClass("_jtD_TagHoverHighlight");
+          jQuery("#_jtD_TagButtonEditId" + id).show();
+          jQuery("#_jtD_TagButtonNewId" + id).show();
+        });
+        jQuery("._jtD_MainBox").bind("mouseout", function(data){
+          var id = _jtD_getId(jQuery(this).attr("id"), _jtD_MainBoxId);
+          jQuery("#_jtD_TagTitleId" + id).removeClass("_jtD_TagHoverHighlight");
+          jQuery("#_jtD_TagButtonEditId" + id).hide();
+          jQuery("#_jtD_TagButtonNewId" + id).hide();
+        });
+        
+        isPinned = 1;
+        curPinnedId = _jtD_getId(jQuery(this).attr("id"), _jtD_MainBoxId);
+        
+        jQuery("#_jtD_TagTitleId" + curPinnedId).removeClass("_jtD_TagHoverHighlight");
+        jQuery("#_jtD_TagButtonEditId" + curPinnedId).hide();
+        jQuery("#_jtD_TagButtonNewId" + curPinnedId).hide();
+        jQuery("#_jtD_TagTitleId" + curPinnedId).addClass("_jtD_TagClickedHighlight");
+        
+      }
+    });
+    
+    jQuery("._jtD_MainBox").bind("mouseover", function(data){
+      var id = _jtD_getId(jQuery(this).attr("id"), _jtD_MainBoxId);
+      jQuery("#_jtD_DetailBoxId" + id).show();
+      jQuery("#_jtD_TagTitleId" + id).addClass("_jtD_TagHoverHighlight");
+      jQuery("#_jtD_TagButtonEditId" + id).show();
+      jQuery("#_jtD_TagButtonNewId" + id).show();
+    });
 
-		jQuery("._jtD_MainBox").bind("mouseout", function(data){
-			var id = _jtD_getId(jQuery(this).attr("id"), _jtD_MainBoxId);
-			jQuery("#_jtD_DetailBoxId" + id).hide();
-			jQuery("#_jtD_TagTitleId" + id).removeClass("_jtD_TagHoverHighlight");
-			jQuery("#_jtD_TagButtonEditId" + id).hide();
-			jQuery("#_jtD_TagButtonNewId" + id).hide();
-		});
-		
-		jQuery("._jtD_Button").bind("mouseover", function(data){
-		  jQuery(this).addClass("_jtD_ButtonHighlight");
-		});
-		
-		jQuery("._jtD_Button").bind("mouseout", function(data){
-		  jQuery(this).removeClass("_jtD_ButtonHighlight");
+    jQuery("._jtD_MainBox").bind("mouseout", function(data){
+      var id = _jtD_getId(jQuery(this).attr("id"), _jtD_MainBoxId);
+      jQuery("#_jtD_DetailBoxId" + id).hide();
+      jQuery("#_jtD_TagTitleId" + id).removeClass("_jtD_TagHoverHighlight");
+      jQuery("#_jtD_TagButtonEditId" + id).hide();
+      jQuery("#_jtD_TagButtonNewId" + id).hide();
+    });
+    
+    jQuery("._jtD_Button").bind("mouseover", function(data){
+      jQuery(this).addClass("_jtD_ButtonHighlight");
+    });
+    
+    jQuery("._jtD_Button").bind("mouseout", function(data){
+      jQuery(this).removeClass("_jtD_ButtonHighlight");
     });
     
     jQuery("._jtD_TagButtonEdit").bind("click", function(data){
